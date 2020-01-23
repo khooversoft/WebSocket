a4f4a7a30b5054f90f4a0fb9919054a194717b24
Kelvin <v-kelhoo@microsoft.com>
2020-01-23 00:48:58 -0800

    Initial setup

---
+++ Test/WebSocketClient/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketClient
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await new Program().RunWebSockets();
+        }
+
+        private async Task RunWebSockets()
+        {
+            using var client = new ClientWebSocket();
+            await client.ConnectAsync(new Uri("ws://localhost:5000/Chat"), CancellationToken.None);
+
+            Console.WriteLine("Connected!");
+
+            var sending = Task.Run(async () =>
+            {
+                string line;
+                while ((line = Console.ReadLine()) != null && line != String.Empty)
+                {
+                    var bytes = Encoding.UTF8.GetBytes(line);
+                    await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+
+                await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+            });
+
+            var receiving = Receiving(client);
+
+            await Task.WhenAll(sending, receiving);
+        }
+
+        private async Task Receiving(ClientWebSocket client)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (true)
+            {
+                var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                switch (result.MessageType)
+                {
+                    case WebSocketMessageType.Text:
+                        Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, result.Count));
+                        break;
+
+                    case WebSocketMessageType.Close:
+                        if (client.State != WebSocketState.Closed)
+                        {
+                            await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                        }
+                        return;
+                }
+            }
+        }
+    }
+}
